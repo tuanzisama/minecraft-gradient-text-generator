7db50e53e9d08f1879ce18c8bda7f0cf4f00ae74
tuanzi <33369729+tuanzisama@users.noreply.github.com>
2023-06-30 10:10:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "minecraft-color-gradient-generator",
+  plugins: [vue()]
 });
