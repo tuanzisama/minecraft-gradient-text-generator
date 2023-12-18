942168826e22034b35fe9c8cf42bf61c5b833553
evenwan <evenwan0010@163.com>
2023-12-18 14:55:58 +0800

    feat: add code source

--- src/components/mcg-layout/footer.vue
+++ src/components/mcg-layout/footer.vue
@@ -1,7 +1,11 @@
 <template>
   <footer class="cg-footer">
     <t-space direction="vertical" size="10px">
-      <t-link href="https://github.com/tuanzisama" target="_blank" theme="primary" style="font-weight: bold"> Github@tuanzisama </t-link>
+      <div>
+        <t-link href="https://github.com/tuanzisama" target="_blank" theme="primary" style="font-weight: bold"> @tuanzisama </t-link>
+        <t-divider layout="vertical" />
+        <t-link href="https://github.com/tuanzisama/minecraft-color-gradient-generator" target="_blank" theme="primary" style="font-weight: bold"> Code source </t-link>
+      </div>
       <t-link href="https://www.mcbbs.net/thread-1004671-1-1.html" target="_blank" theme="warning">
         [广告] PlayerTitle × 一款有点好用的玩家称号插件
       </t-link>
@@ -17,3 +21,4 @@
   padding: 40px 0;
 }
 </style>
+``
