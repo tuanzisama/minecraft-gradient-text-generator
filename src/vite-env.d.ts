d2020eba507e210f8a5c2002b3bbc474d29d22b8
--global <evenwan0010@163.com>
2024-07-20 18:10:11 +0800

    chore: add enviorment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,18 @@
 /// <reference types="vite/client" />
 
 declare module "color-stepper";
+
+interface ImportMetaEnv {
+  /**
+   * Google Analytics Code
+   */
+  readonly VITE_GOOGLE_ANALYTICS: string;
+  /**
+   * Baidu Tongji Code
+   */
+  readonly VITE_BAIDU_TONGJI: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
